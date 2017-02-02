247908cafdecae2d27c1f22e27a365d712bfd83e
yagator <yagator@gmail.com>
2017-02-01 21:04:48 -0600

    scroll

--- public_html/js/base.js
+++ public_html/js/base.js
@@ -1,4 +1,20 @@
-$(document).ready(function (){
+$(document).ready(function () {
     //Aqui va todo el codigo Javascript y jQuery
-    
+    $("header#home nav ul li a.menu").click(function () {
+        var link = $(this).attr("href");
+        var destination = $(link), y = 0, offset;
+        if (link != "" && destination.length) {
+            offset = destination.offset();
+            if (offset != null) {
+                y = offset.top;
+                scroll(y);
+            }
+        }
+        
+        return false;
+    });
 });
+
+function scroll(y) {
+    $("html,body").animate({scrollTop: y}, 2500);
+}
